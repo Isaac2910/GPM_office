31283c30e06ee3e0dda901d30e2afbbe42efd284
Newton10 <isaacomar29@gmail.com>
2025-09-20 20:55:00 +0100

    V0

--- GPM/urls.py
+++ GPM/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda r: redirect('authentication:login')),
     path('auth/', include('authentication.urls', namespace='auth')),
-    #path('ptg', include('pointage.urls'))
+    path('ptg', include('pointage.urls', namespace='pointage' ))
 ]
